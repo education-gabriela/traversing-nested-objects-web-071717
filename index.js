d89b4b612d8c3714fa6361fb55766020d96b4047
gabidavila <gabidavila@gmail.com>
2017-08-29 11:12:56 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function find(array, criteriaFn) {
+  let current = array;
+  let next = [];
+
+  while (current) {
+    console.log(current)
+    if (criteriaFn(current)) {
+      return current;
+    }
+  }
+
+  if (Array.isArray(current)) {
+    for (let i = 0; i < current.length; i++) {
+      next.push(current[i]);
+    }
+  }
+
+  current = next.shift();
+}
